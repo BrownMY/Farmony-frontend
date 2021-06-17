cda435844b6870b364d703e00215877a44204620
mbrown <tugtuj@gmail.com>
2021-06-17 17:45:36 -0400

    corrected config for deleted files

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,13 +5,10 @@ import NewPost from '../components/NewPost'
 import EditForm from '../components/EditForm'
 import ViewPost from '../components/ViewPost'
 import GardenProfile from '../components/GardenProfile'
-import NewPostBuy from '../components/NewPostBuy'
 import ViewPostBuy from '../components/ViewPostBuy'
 import ViewPostTrade from '../components/ViewPostTrade'
-import NewPostTrade from '../components/NewPostTrade'
 import OtherProfiles from '../components/OtherProfiles'
 import ViewPostVolunteer from '../components/ViewPostVolunteer'
-import NewPostVolunteer from '../components/NewPostVolunteer'
 import Volunteer from '../components/Volunteer'
 
 
@@ -21,17 +18,14 @@ export default(
         <Route exact path='/' component={ Home } />
         <Route exact path='/gardens' component={ Gardens } />
         <Route exact path='/newpost' component={ NewPost } />
-        <Route exact path='/newpostbuy' component={ NewPostBuy } />
         <Route exact path='/editform' component={ EditForm } />
         <Route exact path='/post/:id' component={ ViewPost } />
         <Route exact path='/viewpost/:id' component={ ViewPost } />
         <Route exact path='/viewpostbuy/:id' component={ ViewPostBuy } />
         <Route exact path='/viewposttrade/:id' component={ ViewPostTrade } />
         <Route exact path='/viewposttrade' component={ ViewPostTrade } />
-        <Route exact path='/newposttrade' component={ NewPostTrade } />
         <Route exact path='/gardenprofile/:id'  component={GardenProfile} />
         <Route exact path='/viewpostvolunteer/:id'  component={ViewPostVolunteer}/>
-        <Route exact path='/newpostvolunteer'  component={NewPostVolunteer}/>
         <Route exact path='/previewprofile/:id'  component={OtherProfiles} />
         <Route exact path='/volunteer' component={Volunteer} />
     </Switch> 
